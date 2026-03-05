ed7facf615cd75934e69b903188918ed43c7fd22
P. Michael Kielstra <4313413+PMKielstra@users.noreply.github.com>
2026-03-04 17:55:15 -0800

    Cleanup and formatting

--- common.jl
+++ common.jl
@@ -5,11 +5,11 @@ function cannot_split(X, sz)
 end
 
 function split_2d(X, sz)
-    r_len, c_len = size(X)    
+    r_len, c_len = size(X)
     r_step = r_len ÷ sz[1]
     c_step = c_len ÷ sz[2]
-    r_ranges = [((i-1)*r_step + 1):(i == sz[1] ? r_len : i*r_step) for i in 1:sz[1]]
-    c_ranges = [((j-1)*c_step + 1):(j == sz[2] ? c_len : j*c_step) for j in 1:sz[2]]
+    r_ranges = [((i-1)*r_step+1):(i==sz[1] ? r_len : i*r_step) for i = 1:sz[1]]
+    c_ranges = [((j-1)*c_step+1):(j==sz[2] ? c_len : j*c_step) for j = 1:sz[2]]
     [view(X, rr, cr) for rr in r_ranges, cr in c_ranges]
 end
 
@@ -24,7 +24,6 @@ function make_recursive_multiplier(multiplier, A_shape, B_shape)
         next_B = split_2d(B, B_shape)
         multiplier(rmul, next_A, next_B)
     end
-    rmul
 end
 
 export test_multiply
